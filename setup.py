07b82be2456ffb5cc22d1e8417089127c05817f3
Doc Tart <doc.tart@wookalar.io>
2016-10-11 22:35:29 -0400

    Added .travis.yml

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name='slashlock',
-    version='0.1.0',
+    version='0.1.1',
     description='Slashlock - A Simple File and Folder Encryptor',
-    url='',
+    url='https://github.com/wookalar/slashlock/blob/master/slashlock.py',
     author='Wookalar Software',
     author_email='doc.tart@wookalar.io',
     license='AGPLv3',
@@ -12,13 +12,13 @@ setup(
         'slashlock = slashlock:main',
     ]},
     install_requires=[
-        'argon2-cffi==16.2.0',
-        'cffi==1.8.3',
-        'pkg-resources==0.0.0',
-        'pycparser==2.14',
-        'PyNaCl==1.0.1',
-        'six==1.10.0',
+        'argon2-cffi>=16.2.0',
+        'cffi>=1.8.3',
+        'pkg-resources>=0.0.0',
+        'pycparser>=2.14',
+        'PyNaCl>=1.0.1',
+        'six>=1.10.0',
     ],
-    install_package_data=True,
+    install_package_data=False,
     py_modules=['slashlock'],
 )
