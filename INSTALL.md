81ef882999c3947b808a02df8317afc50e695893
Doc Tart <doc.tart@wookalar.io>
2016-10-11 22:47:10 -0400

    Fixing setup.py

---
+++ INSTALL.md
@@ -0,0 +1,20 @@
+1. Install dependencies
+
+sudo apt-get install -y \
+    build-essential \
+    python3-dev \
+    ffmpeg \
+    libsdl2-dev \
+    libsdl2-image-dev \
+    libsdl2-mixer-dev \
+    libsdl2-ttf-dev \
+    libportmidi-dev \
+    libswscale-dev \
+    libavformat-dev \
+    libavcodec-dev \
+    zlib1g-dev \
+    libffi-dev
+
+2. Install requirements
+
+pip install -r requirements.txt
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='slashlock',
-    version='0.1.1',
+    version='0.1.2',
     description='Slashlock - A Simple File and Folder Encryptor',
     url='https://github.com/wookalar/slashlock/blob/master/slashlock.py',
     author='Wookalar Software',
@@ -13,11 +13,7 @@ setup(
     ]},
     install_requires=[
         'argon2-cffi>=16.2.0',
-        'cffi>=1.8.3',
-        'pkg-resources>=0.0.0',
-        'pycparser>=2.14',
         'PyNaCl>=1.0.1',
-        'six>=1.10.0',
     ],
     install_package_data=False,
     py_modules=['slashlock'],
